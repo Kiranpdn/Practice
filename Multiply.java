9d652cc341425da27db7c44865bd3fc699c0c8d5
kiranpdn <kirandhandare12@gmail.com>
2023-07-25 14:11:02 +0530

    Changed Code

--- Multiply.java
+++ Multiply.java
@@ -2,6 +2,6 @@ public class Multiply{
     public static void main(String [] args){
         int a=3;
         int b = 5;
-        System.out.println("Multi" + a*b);
+        System.out.println("Multi of two numbers" + a*b);
     }
 }
