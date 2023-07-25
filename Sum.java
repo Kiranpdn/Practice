24b5a704ba4df03435625d7d4f3bf14f013735eb
kiranpdn <kirandhandare12@gmail.com>
2023-07-25 13:54:29 +0530

    ok

--- Sum.java
+++ Sum.java
@@ -2,6 +2,6 @@ public class Sum{
     public static void main(String [] args){
         int a=3;
         int b = 5;
-        System.out.println(a+b);
+        System.out.println("Sum of" + a+b);
     }
 }
